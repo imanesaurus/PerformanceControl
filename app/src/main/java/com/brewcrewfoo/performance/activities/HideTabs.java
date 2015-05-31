8e5351ace4ec65f6fb4e1312c1458ad3f064d9d4
Firmansyah Otoluwa <pirmansah.otoluwa@gmail.com>
2015-06-01 02:15:24 +0800

    Initial commit

---
+++ app/src/main/java/com/brewcrewfoo/performance/activities/HideTabs.java
@@ -0,0 +1,136 @@
+package com.brewcrewfoo.performance.activities;
+
+/**
+ * Created by h0rn3t on 22.10.2013.
+ */
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.brewcrewfoo.performance.R;
+import com.brewcrewfoo.performance.util.ActivityThemeChangeInterface;
+import com.brewcrewfoo.performance.util.Constants;
+import com.brewcrewfoo.performance.util.DragSortListView;
+import com.brewcrewfoo.performance.util.Helpers;
+import com.brewcrewfoo.performance.util.Tab;
+import com.brewcrewfoo.performance.util.TabAdapter;
+
+import java.util.ArrayList;
+
+
+public class HideTabs extends Activity implements Constants, ActivityThemeChangeInterface {
+    private boolean mIsLightTheme;
+    SharedPreferences mPreferences;
+    private final Context context=this;
+    private TabAdapter dataAdapter;
+
+
+    private DragSortListView.DropListener onDrop =
+            new DragSortListView.DropListener() {
+                @Override
+                public void drop(int from, int to) {
+                    if (from != to) {
+                        Tab item = dataAdapter.getItem(from);
+                        dataAdapter.remove(item);
+                        dataAdapter.insert(item, to);
+                        dataAdapter.notifyDataSetChanged();
+                        String s="";
+                        for(int i=0;i<getResources().getStringArray(R.array.tabs).length;i++){
+                            s+=dataAdapter.getItem(i).getId()+":";
+                        }
+
+                        mPreferences.edit().putString("tab_ids",s).apply();
+                        MainActivity.thide=true;
+                    }
+                }
+            };
+    private DragSortListView.RemoveListener onRemove =
+            new DragSortListView.RemoveListener() {
+                @Override
+                public void remove(int which) {
+                    dataAdapter.remove(dataAdapter.getItem(which));
+                }
+            };
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        setTheme();
+        setContentView(R.layout.hide_tabs);
+        DragSortListView listView = (DragSortListView) findViewById(R.id.applist);
+        listView.setDropListener(onDrop);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Tab t = (Tab) parent.getItemAtPosition(position);
+                t.setSelected(!t.isSelected());
+                dataAdapter.notifyDataSetChanged();
+            }
+        });
+        ArrayList<Tab> TabList = new ArrayList<Tab>();
+
+        String def_ids="";
+        for(int i=0;i<getResources().getStringArray(R.array.tabs).length;i++) def_ids+=i+":";
+        final String Tids=mPreferences.getString("tab_ids",def_ids);
+        for(int i=0;i<getResources().getStringArray(R.array.tabs).length;i++) {
+            String tid=Tids.split(":")[i];
+            if((tid!=null)&&(!tid.equals(""))) {
+                int id=Integer.valueOf(tid);
+                final String sTab=getResources().getStringArray(R.array.tabs)[id];
+                TabList.add(new Tab(sTab, mPreferences.getBoolean(sTab, true), id));
+            }
+        }
+
+        /*for(int i=0;i<getResources().getStringArray(R.array.tabs).length;i++) {
+            Tab t = new Tab(getResources().getStringArray(R.array.tabs)[i],mPreferences.getBoolean(getResources().getStringArray(R.array.tabs)[i],true),i);
+            if(Helpers.is_Tab_available(i)) TabList.add(t);
+        }*/
+        dataAdapter = new TabAdapter(this,R.layout.tab_item, TabList);
+        listView.setAdapter(dataAdapter);
+
+    }
+    @Override
+    public boolean isThemeChanged() {
+        final boolean is_light_theme = mPreferences.getBoolean(PREF_USE_LIGHT_THEME, false);
+        return is_light_theme != mIsLightTheme;
+    }
+
+    @Override
+    public void setTheme() {
+        final boolean is_light_theme = mPreferences.getBoolean(PREF_USE_LIGHT_THEME, false);
+        mIsLightTheme = is_light_theme;
+        setTheme(is_light_theme ? R.style.Theme_Light : R.style.Theme_Dark);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        boolean flag=false;
+        boolean changed=false;
+        for(int i=0;i<dataAdapter.getCount();i++){
+            Tab t = dataAdapter.getItem(i);
+            if(mPreferences.getBoolean(t.getName(),true)!=t.isSelected()) changed=true;
+            if(t.isSelected()) flag=true;
+            mPreferences.edit().putBoolean(t.getName(), t.isSelected()).apply();
+        }
+        if(!flag){
+            // at least one tab (tab0) must be visible
+            Tab t = dataAdapter.getItem(0);
+            mPreferences.edit().putBoolean(t.getName(),true).apply();
+        }
+        if(changed){
+            MainActivity.thide=true;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/brewcrewfoo/performance/activities/MainActivity.java
@@ -0,0 +1,301 @@
+package com.brewcrewfoo.performance.activities;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.brewcrewfoo.performance.R;
+import com.brewcrewfoo.performance.fragments.*;
+import com.brewcrewfoo.performance.util.ActivityThemeChangeInterface;
+import com.brewcrewfoo.performance.util.BootClass;
+import com.brewcrewfoo.performance.util.Constants;
+import com.brewcrewfoo.performance.util.Helpers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends Activity implements Constants,ActivityThemeChangeInterface {
+    public static Context c;
+    public static Boolean thide=false;
+    public static ArrayList<String> mCurGovernor = new ArrayList<String>();
+    public static ArrayList<String> mCurIO = new ArrayList<String>();
+    public static ArrayList<String> mMaxFreqSetting = new ArrayList<String>();
+    public static ArrayList<String> mMinFreqSetting = new ArrayList<String>();
+    public static ArrayList<String> mCPUon = new ArrayList<String>();
+    public static String[] mAvailableFrequencies = new String[0];
+    public static int curcpu=0;
+    public static boolean is_restored=false;
+    private SharedPreferences mPreferences;
+    private ViewPager mViewPager;
+    private boolean mIsLightTheme;
+    private boolean pref_changed=false;
+    private PreferenceChangeListener mPreferenceListener;
+    private TitleAdapter titleAdapter;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        c=this;
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        setTheme();
+        setContentView(R.layout.activity_main);
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        PagerTabStrip mPagerTabStrip = (PagerTabStrip) findViewById(R.id.pagerTabStrip);
+
+        //mPagerTabStrip.setBackgroundColor(getResources().getColor(R.color.pc_light_gray));
+        mPagerTabStrip.setTabIndicatorColor(getResources().getColor(android.R.color.holo_blue_light));
+
+        mPagerTabStrip.setDrawFullUnderline(true);
+        titleAdapter = new TitleAdapter(getFragmentManager());
+        if(savedInstanceState==null) {
+            checkForSu();
+        }
+        else {
+
+            mViewPager.setAdapter(titleAdapter);
+            mViewPager.setCurrentItem(0);
+        }
+        mPreferenceListener = new PreferenceChangeListener();
+        mPreferences.registerOnSharedPreferenceChangeListener(mPreferenceListener);
+    }
+    @Override
+    public void onSaveInstanceState(Bundle saveState) {
+        super.onSaveInstanceState(saveState);
+    }
+
+    /**
+     * Get a list of titles for the tabstrip to display depending on if the
+     * @return String[] containing titles
+     */
+    private String[] getTitles() {
+        List<String> titleslist = new ArrayList<String>();
+        String def_ids="";
+        for(int i=0;i<getResources().getStringArray(R.array.tabs).length;i++) def_ids+=i+":";
+        final String Tids=mPreferences.getString("tab_ids",def_ids);
+
+        for(int i=0;i<getResources().getStringArray(R.array.tabs).length;i++) {
+            String tid=Tids.split(":")[i];
+            if((tid!=null)&&(!tid.equals(""))) {
+                int id = Integer.valueOf(tid);
+                final String sTab=getResources().getStringArray(R.array.tabs)[id];
+                boolean isvisible = mPreferences.getBoolean(sTab, true);
+                if (Helpers.is_Tab_available(id) && isvisible) titleslist.add(sTab);
+            }
+        }
+        return titleslist.toArray(new String[titleslist.size()]);
+    }
+
+    class TitleAdapter extends FragmentPagerAdapter {
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        public TitleAdapter(FragmentManager fm) {
+            super(fm);
+
+            String def_ids="";
+            for(int i=0;i<getResources().getStringArray(R.array.tabs).length;i++) def_ids+=i+":";
+            final String Tids=mPreferences.getString("tab_ids",def_ids);
+
+            int j=0;
+            for(int i=0;i<getResources().getStringArray(R.array.tabs).length;i++) {
+                String tid=Tids.split(":")[i];
+                if((tid!=null)&&(!tid.equals(""))) {
+                    int id=Integer.valueOf(tid);
+                    boolean isvisible = mPreferences.getBoolean(getResources().getStringArray(R.array.tabs)[id], true);
+                    if (Helpers.is_Tab_available(id) && isvisible) {
+                        switch (id) {
+                            case 0:
+                                frags[j] = new CPUSettings();
+                                break;
+                            case 1:
+                                frags[j] = new CPUAdvanced();
+                                break;
+                            case 2:
+                                frags[j] = new BatteryInfo();
+                                break;
+                            case 3:
+                                frags[j] = new MemSettings();
+                                break;
+                            case 4:
+                                frags[j] = new VoltageControlSettings();
+                                break;
+                            case 5:
+                                frags[j] = new Advanced();
+                                break;
+                            case 6:
+                                frags[j] = new TimeInState();
+                                break;
+                            case 7:
+                                frags[j] = new DiskInfo();
+                                break;
+                            case 8:
+                                frags[j] = new Tools();
+                                break;
+                        }
+                        j++;
+                    }
+                }
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+    @Override
+    public void onDestroy() {
+        mPreferences.unregisterOnSharedPreferenceChangeListener(mPreferenceListener);
+        super.onDestroy();
+    }
+    @Override
+    public void onStop() {
+        if(mPreferences.getBoolean("boot_mode",false) && pref_changed){
+            new Thread(new Runnable() {
+                public void run(){
+                    new BootClass(c,mPreferences).writeScript();
+                }
+            }).start();
+            Toast.makeText(c, "init.d script updated", Toast.LENGTH_SHORT).show();
+        }
+        super.onStop();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (isThemeChanged() || thide || is_restored) {
+            if(thide) thide=false;
+            if(is_restored) is_restored=false;
+            Helpers.restartPC(this);
+        }
+
+    }
+
+
+
+    @Override
+    public boolean isThemeChanged() {
+        final boolean is_light_theme = mPreferences.getBoolean(PREF_USE_LIGHT_THEME, false);
+        return is_light_theme != mIsLightTheme;
+    }
+
+    @Override
+    public void setTheme() {
+        final boolean is_light_theme = mPreferences.getBoolean(PREF_USE_LIGHT_THEME, false);
+        mIsLightTheme = mPreferences.getBoolean(PREF_USE_LIGHT_THEME, false);
+        setTheme(is_light_theme ? R.style.Theme_Light : R.style.Theme_Dark);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if ((requestCode == 1)&&(resultCode == RESULT_OK)) {
+            String r= data.getStringExtra("r");
+            if(r!=null && r.equals("ok")){
+                mPreferences.edit().putString("rom",Build.DISPLAY).commit();
+                getCPUval();
+                mViewPager.setAdapter(titleAdapter);
+                mViewPager.setCurrentItem(0);
+                return;
+            }
+        }
+        finish();
+    }
+    private void checkForSu() {
+        if(mPreferences.getBoolean("theme_changed",false)){
+            mPreferences.edit().putBoolean("theme_changed",false).commit();
+            getCPUval();
+            mViewPager.setAdapter(titleAdapter);
+            mViewPager.setCurrentItem(0);
+        }
+        else {
+            final String b=mPreferences.getString("rom","");
+            if(!b.equals(Build.DISPLAY)) {
+                Log.d(TAG, "check for su & busybox");
+                Intent intent = new Intent(MainActivity.this, checkSU.class);
+                startActivityForResult(intent, 1);
+            }
+            else{
+                if(Helpers.checkSu()) {
+                    getCPUval();
+                    mViewPager.setAdapter(titleAdapter);
+                    mViewPager.setCurrentItem(0);
+                }
+                else{
+                    Log.d(TAG, "check for su");
+                    Intent intent = new Intent(MainActivity.this, checkSU.class);
+                    startActivityForResult(intent, 1);
+                }
+            }
+        }
+    }
+    public static void getCPUval(){
+        final int nCpus = Helpers.getNumOfCpus();
+        final String r=Helpers.readCPU(c, nCpus);
+        Log.d(TAG, "utils read: " + r);
+        if(r.contains(":")){
+            mAvailableFrequencies = r.split(":")[nCpus * 5].split(" ");
+            mMaxFreqSetting.clear();
+            mMinFreqSetting.clear();
+            mCurGovernor.clear();
+            mCurIO.clear();
+            mCPUon.clear();
+            for (int i = 0; i < nCpus; i++){
+                if(Integer.parseInt(r.split(":")[i*5])<0)
+                    mMinFreqSetting.add(i,mAvailableFrequencies[0]);
+                else
+                    mMinFreqSetting.add(i,r.split(":")[i*5]);
+
+                if(Integer.parseInt(r.split(":")[i*5+1])<0)
+                    mMaxFreqSetting.add(i,mAvailableFrequencies[mAvailableFrequencies.length-1]);
+                else
+                    mMaxFreqSetting.add(i,r.split(":")[i*5+1]);
+
+                /*if(new File(HARD_LIMIT_PATH).exists()){
+                    mMaxFreqSetting[i]=Helpers.readOneLine(HARD_LIMIT_PATH);
+                }*/
+
+                mCurGovernor.add(i,r.split(":")[i*5+2]);
+                mCurIO.add(i,r.split(":")[i*5+3]);
+                mCPUon.add(i,r.split(":")[i*5+4]);
+            }
+        }
+    }
+    private class PreferenceChangeListener implements SharedPreferences.OnSharedPreferenceChangeListener {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+            //Toast.makeText(c, "Changed: " + key, Toast.LENGTH_LONG).show();
+            pref_changed=true;
+            Helpers.updateAppWidget(c);
+        }
+    }
+}
+
